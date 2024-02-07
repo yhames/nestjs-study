615fcb31f6fcf4aae2cd888420c1fa5693acdf81
yhames <wjddn494@naver.com>
2024-02-07 16:40:48 +0900

    :fire: reset and change purpose

--- 02_Refactoring/Calculator02/CalculatorTest.java
+++
@@ -1,9 +0,0 @@
-public class CalculatorTest {
-    public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        System.out.println(cal.add(1, 2));
-        System.out.println(cal.subtract(1, 2));
-        System.out.println(cal.multiply(1, 2));
-        System.out.println(cal.divide(1, 2));
-    }
-}
