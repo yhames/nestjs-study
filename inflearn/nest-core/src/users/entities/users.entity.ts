41a8c0461220b297c88df37279cc2f033c3b1a5c
yhames <wjddn494@naver.com>
2024-02-08 16:01:03 +0900

    :art: add Enum Column

--- inflearn/nest-core/src/users/entities/users.entity.ts
+++ inflearn/nest-core/src/users/entities/users.entity.ts
@@ -1,7 +1,8 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { RolesEnum } from '../const/roles.const';
 
 @Entity()
-export class User {
+export class UserModel {
   @PrimaryGeneratedColumn()
   id: number;
 
@@ -18,4 +19,10 @@ export class User {
 
   @Column()
   password: string;
+
+  @Column({
+    enum: Object.values(RolesEnum),
+    default: RolesEnum.USER,
+  })
+  role: RolesEnum;
 }
