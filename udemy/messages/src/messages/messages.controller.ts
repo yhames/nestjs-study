64655a0dd277e0031dfe1ea14bf859c6c44c6ba5
yhames <wjddn494@naver.com>
2024-02-09 13:54:18 +0900

    ✨ [Udemy] add messages Service and Repository

--- udemy/messages/src/messages/messages.controller.ts
+++ udemy/messages/src/messages/messages.controller.ts
@@ -1,20 +1,28 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // DON'T DO THIS ON REAL APPS
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {
-    return 'All messages';
+  async listMessages() {
+    return this.messagesService.findAll();
   }
 
   @Post()
-  createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+  async createMessage(@Body() body: CreateMessageDto) {
+    await this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    console.log(id);
+  async getMessage(@Param('id') id: string) {
+    return this.messagesService.findOne(id);
   }
 }
