123ed6617b441799b52f7b8c2753838e7654d531
yhames <wjddn494@naver.com>
2024-02-13 02:28:53 +0900

    ✨ add Custom Pipe

--- inflearn/nest-core/src/auth/auth.controller.ts
+++ inflearn/nest-core/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post, Headers } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PasswordPipe } from './pipe/password.pipe';
 
 @Controller('auth')
 export class AuthController {
@@ -30,7 +31,7 @@ export class AuthController {
   async postRegisterEmail(
     @Body('nickname') nickname: string,
     @Body('email') email: string,
-    @Body('password') password: string,
+    @Body('password', PasswordPipe) password: string,
   ) {
     return this.authService.registerWithEmail({ nickname, email, password });
   }
---
+++ inflearn/nest-core/src/auth/pipe/password.pipe.ts
@@ -0,0 +1,25 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+
+/**
+ * CustomPipe를 생성하기 위해서는 `PipeTransform` 인터페이스를 구현해야 한다.
+ */
+@Injectable()
+export class PasswordPipe implements PipeTransform {
+  /**
+   * ArgumentMetadata: 파이프가 적용되는 파라미터에 대한 정보를 포함한다.
+   *  - type: 파라미터의 타입 (body, query, param, custom)
+   *  - metatype: routeHandler 메서드의 타입 정의에 기반한 파라미터의 기본 타입
+   *  - data: 데코레이터에 전달된 문자열
+   */
+  transform(value: any, metadata: ArgumentMetadata): any {
+    if (value.toString().length < 8) {
+      throw new BadRequestException('비밀번호는 8자 이하로 입력하주세요.');
+    }
+    return value.toString();
+  }
+}
