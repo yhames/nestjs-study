37b45a782940a9f19ee235565a233a2147d773ee
yhames <wjddn494@naver.com>
2024-02-13 18:23:01 +0900

    ✨ [Udemy] add createUserDto with Pipe

--- udemy/mycv/src/users/users.controller.ts
+++ udemy/mycv/src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor() {}
+
+  @Post('signup')
+  async createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+  }
+}
