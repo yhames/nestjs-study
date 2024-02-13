6774eea62717edd8999d08144b1c5393e00c4b2c
yhames <wjddn494@naver.com>
2024-02-13 18:35:49 +0900

    :art: add create feat

--- udemy/mycv/src/users/users.controller.ts
+++ udemy/mycv/src/users/users.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor() {}
+  constructor(private readonly usersService: UsersService) {}
 
   @Post('signup')
   async createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    return this.usersService.create(body.email, body.password);
   }
 }
--- udemy/mycv/src/users/users.service.ts
+++ udemy/mycv/src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    /**
+     * 컨테이너에서 의존성 주입을 할 때
+     * 제네릭에 대해서는 주입이 자동으로 되지 않는다.
+     * 따라서 @InjectRepository() 데코레이터를 사용해서 주입해야 한다.
+     */
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create(email: string, password: string) {
+    const user = this.usersRepository.create({ email, password });
+    return this.usersRepository.save(user);
+  }
+}
