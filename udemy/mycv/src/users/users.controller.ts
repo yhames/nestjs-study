3cc65643bd49627c67a35c22105ab70f53ee15bd
yhames <wjddn494@naver.com>
2024-02-14 11:40:57 +0900

    ✨ add UserController

--- udemy/mycv/src/users/users.controller.ts
+++ udemy/mycv/src/users/users.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +18,25 @@ export class UsersController {
   @Post('signup')
   async createUser(@Body() body: CreateUserDto) {
     return this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id, 10));
+  }
+
+  @Get('/:email')
+  async findAllUsers(@Param('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id, 10), body);
+  }
+
+  @Delete('/:id')
+  async removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id, 10));
   }
 }
