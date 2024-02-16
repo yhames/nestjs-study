a679e08a21965077eb23fbd4ecc20af0434dc29f
yhames <wjddn494@naver.com>
2024-02-16 13:37:45 +0900

    :art: add enableImplicitConversion for auto convert

--- inflearn/nest-core/src/main.ts
+++ inflearn/nest-core/src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
     // ValidationPipe()를 주입하지 않아도 class-validator가 전역적으로 적용된다.
     new ValidationPipe({
       transform: true, // 값이 없는 경우 default value로 변환하는 옵션
+      transformOptions: {
+        enableImplicitConversion: true, // string -> number, boolean 등으로 자동 변환하는 옵션
+      },
     }),
   );
   await app.listen(3000);
