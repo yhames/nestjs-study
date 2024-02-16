a39c2ce40f36d96ae655257f38fbec96a1ff809b
yhames <wjddn494@naver.com>
2024-02-16 14:17:16 +0900

    :art: add last element logic

--- inflearn/nest-core/src/posts/posts.service.ts
+++ inflearn/nest-core/src/posts/posts.service.ts
@@ -42,7 +42,10 @@ export class PostsService {
     });
 
     // 해당되는 포스트가 0개 이상이면, 마지막 포스트를 가져오고, 그렇지 않으면 null을 반환한다.
-    const lastItem = posts.length > 0 ? posts[posts.length - 1] : null;
+    const lastItem =
+      posts.length > 0 && posts.length === dto.take
+        ? posts[posts.length - 1]
+        : null;
 
     const nextUrl = lastItem && new URL(`${PROTOCOL}://${HOST}/posts`);
 
@@ -63,10 +66,10 @@ export class PostsService {
     return {
       data: posts,
       cursor: {
-        after: lastItem?.id,
+        after: lastItem?.id ?? null,
       },
       count: posts.length,
-      next: nextUrl?.toString(),
+      next: nextUrl?.toString() ?? null,
     };
   }
 
