e23f3634305aac1c605fee787ce9e67df7bc7295
yhames <wjddn494@naver.com>
2024-02-16 12:38:54 +0900

    ✨ add ClassSerializerInterceptor and Exclude decorator for password property

--- inflearn/nest-core/src/users/entities/users.entity.ts
+++ inflearn/nest-core/src/users/entities/users.entity.ts
@@ -6,6 +6,7 @@ import { IsEmail, IsString, Length } from 'class-validator';
 import { lengthValidationMessage } from '../../common/validation-message/length-validation.message';
 import { stringValidationMessage } from '../../common/validation-message/string-validation.message';
 import { emailValidationMessage } from '../../common/validation-message/email-validation.message';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class UsersModel extends BaseModel {
@@ -25,6 +26,11 @@ export class UsersModel extends BaseModel {
   @Column()
   @IsString({ message: stringValidationMessage })
   @Length(3, 8, { message: lengthValidationMessage })
+  /**
+   *  req : json to dto -> toClassOnly
+   *  res : dto to json -> toPlainOnly
+   */
+  @Exclude({ toPlainOnly: true }) // 요청을 받을 때는 password를 받고, 응답을 보낼 때는 password를 숨긴다.
   password: string;
 
   @Column({
