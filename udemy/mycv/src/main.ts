861aa5275ed580689b0cd2dcfbc6b3f9f7af4345
yhames <wjddn494@naver.com>
2024-02-21 12:32:44 +0900

    :art: add middleware cookie-session

--- udemy/mycv/src/main.ts
+++ udemy/mycv/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['jeongwpa'], // using encrypt key
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // 유효성 검사를 하지 않는 프로퍼티를 삭제
