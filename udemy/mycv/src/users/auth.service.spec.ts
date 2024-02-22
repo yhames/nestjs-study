430f7b8575cb055f8fcb37b6528e56dba9b75b62
yhames <wjddn494@naver.com>
2024-02-22 16:56:47 +0900

    :white_check_mark: add mockUsersSerivce in test code of AuthService

--- udemy/mycv/src/users/auth.service.spec.ts
+++ udemy/mycv/src/users/auth.service.spec.ts
@@ -1,9 +1,23 @@
 import { AuthService } from './auth.service';
 import { Test } from '@nestjs/testing';
+import { UsersService } from './users.service';
 
 it('can create an instance of AuthService', async () => {
+  // Create a fake copy of the UsersService
+  const mockUsersService = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password }),
+  };
+
   const module = await Test.createTestingModule({
-    providers: [AuthService],
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: mockUsersService,
+      },
+    ],
   }).compile();
 
   const authService = module.get(AuthService);
