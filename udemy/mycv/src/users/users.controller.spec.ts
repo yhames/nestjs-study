c191195b71f8c302ed983141917dfe8942d9e801
yhames <wjddn494@naver.com>
2024-02-22 18:34:07 +0900

    ✅ add controller test

--- udemy/mycv/src/users/users.controller.spec.ts
+++ udemy/mycv/src/users/users.controller.spec.ts
@@ -1,12 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './users.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let mockUsersService: Partial<UsersService>;
+  let mockAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    mockUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'test@test.com',
+          password: 'qwer1234',
+        } as User);
+      },
+      find: (email: string) => {
+        return Promise.resolve([
+          { id: 1, email, password: 'qwer1234' } as User,
+        ]);
+      },
+      // update: () => {},
+      // remove: () => {},
+    };
+    mockAuthService = {
+      // signUp: () => {},
+      // signIn: () => {},
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +50,21 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findALlUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('test@test.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('test@test.com');
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async () => {
+    mockUsersService.findOne = () => null;
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
   });
 });
