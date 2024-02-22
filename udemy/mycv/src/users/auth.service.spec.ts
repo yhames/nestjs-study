4f879a05ed942ed5c85d6463f0523318cff605e4
yhames <wjddn494@naver.com>
2024-02-22 17:09:17 +0900

    :art: add type in mockService

--- udemy/mycv/src/users/auth.service.spec.ts
+++ udemy/mycv/src/users/auth.service.spec.ts
@@ -1,13 +1,15 @@
 import { AuthService } from './auth.service';
 import { Test } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { User } from './users.entity';
 
 it('can create an instance of AuthService', async () => {
   // Create a fake copy of the UsersService
-  const mockUsersService = {
+  // Partial을 사용하여 Mock 객체를 생성할 때 TypeScript가 각 메서드의 반환 타입을 추론할 수 있도록 한다.
+  const mockUsersService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+      Promise.resolve({ id: 1, email, password } as User),
   };
 
   const module = await Test.createTestingModule({
