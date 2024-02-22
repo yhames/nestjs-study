d6ac849b6c955c2db478329c8e8b1683b0ee0f9a
yhames <wjddn494@naver.com>
2024-02-22 17:13:16 +0900

    ♻️ change structure of AuthService

--- udemy/mycv/src/users/auth.service.spec.ts
+++ udemy/mycv/src/users/auth.service.spec.ts
@@ -3,25 +3,32 @@ import { Test } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { User } from './users.entity';
 
-it('can create an instance of AuthService', async () => {
-  // Create a fake copy of the UsersService
-  // Partial을 사용하여 Mock 객체를 생성할 때 TypeScript가 각 메서드의 반환 타입을 추론할 수 있도록 한다.
-  const mockUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+describe('AuthService', () => {
+  let authService: AuthService;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: mockUsersService,
-      },
-    ],
-  }).compile();
+  beforeEach(async () => {
+    // Create a fake copy of the UsersService
+    // Partial을 사용하여 Mock 객체를 생성할 때 TypeScript가 각 메서드의 반환 타입을 추론할 수 있도록 한다.
+    const mockUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const authService = module.get(AuthService);
-  expect(authService).toBeDefined();
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
+    }).compile();
+
+    authService = module.get(AuthService);
+  });
+
+  it('can create an instance of AuthService', async () => {
+    expect(authService).toBeDefined();
+  });
 });
