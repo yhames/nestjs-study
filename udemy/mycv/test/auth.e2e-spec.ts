d5a076def72122268e77a88af2c3163302819cdd
yhames <wjddn494@naver.com>
2024-02-23 17:02:59 +0900

    :test_tube: add auth e2e test

---
+++ udemy/mycv/test/auth.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handls a signup request', () => {
+    const email = 'test@test.com';
+    const password = 'qwer1234';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password })
+      .expect(201)
+      .then((res) => {
+        expect(res.body.id).toBeDefined();
+        expect(res.body.email).toEqual(email);
+      });
+  });
+});
