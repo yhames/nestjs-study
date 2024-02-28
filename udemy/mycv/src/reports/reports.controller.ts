91419a8c529720064b614858049066e590df9a7c
yhames <wjddn494@naver.com>
2024-02-28 11:37:00 +0900

    :art: add association with create api

--- udemy/mycv/src/reports/reports.controller.ts
+++ udemy/mycv/src/reports/reports.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/users.entity';
 
 @Controller('reports')
 export class ReportsController {
@@ -9,7 +11,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    this.reportsService.create(body);
+  async createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
--- udemy/mycv/src/reports/reports.service.ts
+++ udemy/mycv/src/reports/reports.service.ts
@@ -3,6 +3,7 @@ import { CreateReportDto } from './dtos/create-report.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './reports.entity';
 import { Repository } from 'typeorm';
+import { User } from '../users/users.entity';
 
 @Injectable()
 export class ReportsService {
@@ -11,8 +12,8 @@ export class ReportsService {
     private readonly reportRepository: Repository<Report>,
   ) {}
 
-  create(body: CreateReportDto) {
-    const report = this.reportRepository.create(body);
+  create(body: CreateReportDto, user: User) {
+    const report = this.reportRepository.create({ ...body, user });
     return this.reportRepository.save(report);
   }
 }
