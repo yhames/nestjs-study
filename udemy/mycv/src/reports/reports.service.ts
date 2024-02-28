bb4f1716bc7ced7cd0ead27d9febadecdcea6dd1
yhames <wjddn494@naver.com>
2024-02-28 10:47:40 +0900

    ✨ add report create

--- udemy/mycv/src/reports/reports.service.ts
+++ udemy/mycv/src/reports/reports.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './reports.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report)
+    private readonly reportRepository: Repository<Report>,
+  ) {}
+
+  create(body: CreateReportDto) {
+    const report = this.reportRepository.create(body);
+    return this.reportRepository.save(report);
+  }
+}
