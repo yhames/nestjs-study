122591bf3eabcf56ec4d826f709de69a96fa87ed
yhames <wjddn494@naver.com>
2024-03-01 11:19:03 +0900

    :art: add currentUser property in Request interface with Typescript global keyword

--- udemy/mycv/src/users/middlewares/current-user.middleware.ts
+++ udemy/mycv/src/users/middlewares/current-user.middleware.ts
@@ -1,6 +1,16 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../users.entity';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -15,8 +25,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
     const { userId } = req.session || {};
 
     if (userId) {
-      // eslint-disable-next-line
-      // @ts-expect-error
       req.currentUser = await this.usersService.findOne(userId);
     }
 
