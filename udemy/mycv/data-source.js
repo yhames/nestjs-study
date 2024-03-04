c8f0f32b2bbaf259a66ccf1e8dff883877fd968d
yhames <wjddn494@naver.com>
2024-03-04 15:08:30 +0900

    📝 add comments for migration:generate

--- udemy/mycv/data-source.js
+++ udemy/mycv/data-source.js
@@ -3,6 +3,9 @@ const { DataSource } = require('typeorm');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const dbConfig = require('./ormconfig');
 
+/**
+ * migration:generate는 DataSource를 반드시 사용해야 한다.
+ */
 let AppDataSource = new DataSource(dbConfig);
 
 module.exports = AppDataSource;
