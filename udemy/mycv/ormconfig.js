daaca36192387009c0bb7e0547516ae6dd5e303c
yhames <wjddn494@naver.com>
2024-03-04 16:18:48 +0900

    :art: change dbConfig and package.json for production

--- udemy/mycv/ormconfig.js
+++ udemy/mycv/ormconfig.js
@@ -11,18 +11,27 @@ switch (process.env.NODE_ENV) {
     Object.assign(dbConfig, {
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: ['**/*.entity.js'],
+      entities: ['**/*.entity.js'], // local에서는 dist/js를 사용합니다.
     });
     break;
   case 'test':
     Object.assign(dbConfig, {
       type: 'sqlite',
       database: 'test.sqlite',
-      entities: ['**/*.entity.ts'],
+      entities: ['**/*.entity.ts'], // jest는 ./ts를 사용합니다.
       migrationsRun: true,
     });
     break;
   case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationRun: true,
+      entities: ['**/*.entity.js'], // production에서는 dist/js를 사용합니다.
+      ssl: {
+        rejectUnauthorized: false, // heroku에서는 ssl 옵션을 추가해야 합니다.
+      },
+    });
     break;
   default:
     throw new Error('Environment not recognized');
