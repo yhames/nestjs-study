db4a87d1387a8e3aacb1c119f1bf858937d3a6a7
yhames <wjddn494@naver.com>
2024-03-04 16:38:31 +0900

    :art: add config for heroku

--- udemy/mycv/src/main.ts
+++ udemy/mycv/src/main.ts
@@ -1,22 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use(
-  //   cookieSession({
-  //     keys: ['jeongwpa'], // using encrypt key
-  //   }),
-  // );
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true, // 유효성 검사를 하지 않는 프로퍼티를 삭제
-  //   }),
-  // );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
